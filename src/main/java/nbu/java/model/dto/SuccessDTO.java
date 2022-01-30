2cdff91ea4787746b3c1560d7d170fdd5a9163d6
Julieta <julietataseva@gmail.com>
2022-01-30 20:34:24 +0200

    Added SuccessDTO class

---
+++ src/main/java/nbu/java/model/dto/SuccessDTO.java
@@ -0,0 +1,16 @@
+package nbu.java.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Component
+public class SuccessDTO {
+    private String message;
+}
