9accd94f21e48687891cd3680fe5096d50099245
Julieta <julietataseva@gmail.com>
2022-01-30 20:33:06 +0200

    Added LoginUserDTO class

---
+++ src/main/java/nbu/java/model/dto/LoginUserDTO.java
@@ -0,0 +1,15 @@
+package nbu.java.model.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Component
+public class LoginUserDTO {
+    private String username;
+    private String password;
+}
